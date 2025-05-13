0bdca272442c53b8f5968dcfd3fda14cf2ca6b40
Laxman K R <krlaxman03@gmail.com>
2025-05-13 20:26:09 +0530

    Removed github API

--- apps/server/app/trpc/[trpc]/route.ts
+++ apps/server/app/trpc/[trpc]/route.ts
@@ -1,14 +1,33 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from '@/routers';
-import { createContext } from '@/lib/context';
-import { NextRequest } from 'next/server';
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "@/routers";
+import { createContext } from "@/lib/context";
+import { NextRequest, NextResponse } from "next/server";
 
-function handler(req: NextRequest) {
-  return fetchRequestHandler({
-    endpoint: '/trpc',
+const CORS_ORIGIN = process.env.NEXT_PUBLIC_CORS_ORIGIN || "*";
+
+function setCorsHeaders(response: Response) {
+  response.headers.set("Access-Control-Allow-Origin", CORS_ORIGIN);
+  response.headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Authorization"
+  );
+  return response;
+}
+
+async function handler(req: NextRequest) {
+  const response = await fetchRequestHandler({
+    endpoint: "/trpc",
     req,
     router: appRouter,
-    createContext: () => createContext(req)
+    createContext: () => createContext(req),
   });
+  return setCorsHeaders(response as Response);
 }
+
 export { handler as GET, handler as POST };
+
+export async function OPTIONS() {
+  const response = new NextResponse(null, { status: 204 });
+  return setCorsHeaders(response);
+}
--- apps/server/routers/index.ts
+++ apps/server/routers/index.ts
@@ -1,9 +1,7 @@
 import { router } from "../lib/trpc";
 import { testRouter } from "./test";
-import { userRouter } from "./user";
 
 export const appRouter = router({
   test: testRouter,
-  user: userRouter
 });
 export type AppRouter = typeof appRouter;
