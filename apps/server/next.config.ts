ffe32c69c10db16b59b9b60e60e821fdee786dab
Laxman K R <krlaxman03@gmail.com>
2025-05-23 00:41:06 +0530

    Moved `env` variables to `next.config.ts`

--- apps/server/next.config.ts
+++ apps/server/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    CORS_ORIGIN: process.env.NODE_ENV == "development" ? 'http://localhost:300': 'https://www.thaletto.dev',
+
+    // Notion
+    NOTION_TOKEN: process.env.NOTION_TOKEN || "",
+    NOTION_EDUCATION_DB_ID: "1fbf3dd03157801eab9cf98d6cbf4987",
+    NOTION_EXPERIENCE_DB_ID: "1fbf3dd0315780c0bca1cceef54cafd6",
+  }
 };
 
 export default nextConfig;
