48491da0b098c6c9080923fb5220fac2b4703a00
Laxman K R <krlaxman03@gmail.com>
2025-06-07 23:41:08 +0530

    Update package dependencies, enhance header component with avatar and social links, and improve global CSS styles for better aesthetics and functionality.

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,18 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
+import { Button } from './ui/button'
 
 export function Header() {
   return (
     <header className="mb-8 flex items-center justify-between">
-      <div>
-        <Link
-          href="/"
-          className="text-3xl font-medium"
-        >
+      <div className="flex flex-col gap-2">
+        <Link href="/" className="text-3xl font-medium">
           Laxman K R
         </Link>
-        <Link href="#contact">
-          <span className="text-lg font-normal text-zinc-600 dark:text-zinc-400">
-            {' '}
-            @thaletto
-          </span>
-        </Link>
+
         <TextEffect
           as="p"
           preset="fade"
@@ -27,6 +22,15 @@ export function Header() {
         >
           Full Stack AI Developer
         </TextEffect>
+      </div>
+      <div className="flex flex-col items-end gap-2">
+        <Avatar className="size-10">
+          <AvatarImage src="https://github.com/thaletto.png" />
+          <AvatarFallback className='bg-zinc-600 dark:bg-zinc-400 text-zinc-100 dark:text-zinc-900 text-xl'>LKR</AvatarFallback>
+        </Avatar>
+        <Link href="#contact">
+          <p className="text-zinc-600 dark:text-zinc-400">@thaletto</p>
+        </Link>
       </div>
     </header>
   )
