1a137e0e1547dc36f80e5d01fbcc4dfccaae27ed
Laxman K R <krlaxman03@gmail.com>
2025-06-08 17:14:54 +0530

    ADC Logo

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 import { TextEffect } from '@/components/ui/text-effect';
 import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card';
 
 export function Header() {
   return (
@@ -11,15 +12,11 @@ export function Header() {
           Laxman K R
         </Link>
 
-        <TextEffect
-          as="p"
-          preset="fade"
-          per="char"
-          className="text-zinc-600 dark:text-zinc-400"
-          delay={0.5}
-        >
-          Full Stack AI Developer
-        </TextEffect>
+        <div className="text-zinc-600 dark:text-zinc-400">
+          <TextEffect as="div" preset="fade" per="char" delay={0.5}>
+            Full Stack AI Developer
+          </TextEffect>
+        </div>
       </div>
       <div className="flex flex-col items-end gap-2">
         <Avatar className="size-10">
@@ -28,9 +25,20 @@ export function Header() {
             LKR
           </AvatarFallback>
         </Avatar>
-        <Link href="#contact">
-          <p className="text-zinc-600 dark:text-zinc-400">@thaletto</p>
-        </Link>
+        <HoverCard>
+          <HoverCardTrigger className="text-zinc-600 dark:text-zinc-400">
+            @thaletto
+          </HoverCardTrigger>
+          <HoverCardContent className="shadow-lg border-0 text-zinc-600 dark:text-zinc-400 bg-zinc-900 dark:bg-zinc-50 w-full">
+            <div className="flex items-start gap-4">
+              <img src={'/adc.png'} width={50} height={50} />
+              <div className="flex flex-col gap-1">
+                <p className="text-zinc-50 dark:text-zinc-900 font-medium">A Developer Company</p>
+                <p className="text-zinc-400 dark:text-zinc-600">Founder & CEO</p>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </div>
     </header>
   );
