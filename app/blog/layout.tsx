4e140168c657b6b0736bbba0921c3d16b440bcb7
Laxman K R <krlaxman03@gmail.com>
2025-06-08 16:13:52 +0530

    Refactor MDX components for improved readability and consistency; update layout and footer styles for better visual alignment.

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,23 +1,23 @@
-'use client'
-import { TextMorph } from '@/components/ui/text-morph'
-import { ScrollProgress } from '@/components/ui/scroll-progress'
-import { useEffect, useState } from 'react'
+'use client';
+import { TextMorph } from '@/components/ui/text-morph';
+import { ScrollProgress } from '@/components/ui/scroll-progress';
+import { useEffect, useState } from 'react';
 
 function CopyButton() {
-  const [text, setText] = useState('Copy')
-  const currentUrl = typeof window !== 'undefined' ? window.location.href : ''
+  const [text, setText] = useState('Copy');
+  const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
 
   useEffect(() => {
     setTimeout(() => {
-      setText('Copy')
-    }, 2000)
-  }, [text])
+      setText('Copy');
+    }, 2000);
+  }, [text]);
 
   return (
     <button
       onClick={() => {
-        setText('Copied')
-        navigator.clipboard.writeText(currentUrl)
+        setText('Copied');
+        navigator.clipboard.writeText(currentUrl);
       }}
       className="font-base flex items-center gap-1 text-center text-sm text-zinc-500 transition-colors dark:text-zinc-400"
       type="button"
@@ -25,19 +25,15 @@ function CopyButton() {
       <TextMorph>{text}</TextMorph>
       <span>URL</span>
     </button>
-  )
+  );
 }
 
-export default function LayoutBlogPost({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function LayoutBlogPost({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="pointer-events-none fixed top-0 left-0 z-10 h-12 w-full bg-gray-100 to-transparent backdrop-blur-xl [-webkit-mask-image:linear-gradient(to_bottom,black,transparent)] dark:bg-zinc-950" />
       <ScrollProgress
-        className="fixed top-0 z-20 h-0.5 bg-zinc-600 dark:bg-gray-600"
+        className="fixed top-0 z-20 h-1 bg-zinc-600 dark:bg-gray-400"
         springOptions={{
           bounce: 0,
         }}
@@ -46,9 +42,9 @@ export default function LayoutBlogPost({
       <div className="absolute top-40 right-4">
         <CopyButton />
       </div>
-      <div className="prose prose-gray prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium mt-24 w-full max-w-none pb-20">
+      <div className="prose prose-gray prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium prose-hr:border-zinc-500 prose-hr:dark:border-zinc-400 mt-24 w-full max-w-none pb-20">
         {children}
       </div>
     </>
-  )
+  );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -68,7 +68,7 @@ function ThemeSwitch() {
 
 export function Footer() {
   return (
-    <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
+    <footer className="mt-24 border-t border-zinc-500 px-0 py-4 dark:border-zinc-400">
       <div className="flex items-center justify-between">
         <a href="https://github.com/thaletto/thaletto" target="_blank">
           <TextLoop className="text-xs text-zinc-500 dark:text-zinc-400">
