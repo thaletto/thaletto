631bcee28651db4e286be28806f89ffa6448c8f6
Laxman K R <krlaxman03@gmail.com>
2025-06-08 15:57:35 +0530

    Update footer component to link to the new GitHub repository for thaletto.

--- components/footer.tsx
+++ components/footer.tsx
@@ -70,7 +70,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/thaletto/thaletto" target="_blank">
           <TextLoop className="text-xs text-zinc-500 dark:text-zinc-400">
             <span>© {new Date().getFullYear()} thaletto.</span>
             <span>Built with Next.js</span>
