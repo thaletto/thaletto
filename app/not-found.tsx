332f47e6c250b75eed80282941475be0378f46c1
Laxman K R <krlaxman03@gmail.com>
2025-06-09 22:51:34 +0530

    Not Found

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <main className="relative h-[calc(100vh-23rem)] w-full">
+      <div className="flex flex-col justify-center items-center text-center w-full h-full">
+        <h1 className="text-2xl font-regular tracking-tighter">404 - Page Not Found</h1>
+        <p className="mx-auto text-xl text-gray-500 dark:text-gray-400">
+          Oops! The page you're looking for doesn't exist or has been moved.
+        </p>
+        <Button asChild>
+          <Link href="/">Return Home</Link>
+        </Button>
+      </div>
+    </main>
+  );
+}
