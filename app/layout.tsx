22fd67610d5afe9f4e8f113353de76c0c1198746
Laxman K R <krlaxman03@gmail.com>
2025-06-12 20:17:51 +0530

    Fix: Styles not applying in `shadcn/ui` components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import './global.css';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 import { ThemeProvider } from 'next-themes';
