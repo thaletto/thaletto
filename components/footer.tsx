0772ff5ba833e1593fe8c457b22c3285cceb69bb
Laxman K R <krlaxman03@gmail.com>
2025-08-30 17:00:36 +0530

    Updated footer component to enhance text color for better visibility.

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="mt-24 px-0 py-4">
       <div className="flex h-auto flex-row items-center justify-between">
         <a href="https://github.com/a-developer-company" target="_blank">
-          <span className="flex items-center gap-2 text-sm text-zinc-500">
+          <span className="flex items-center gap-2 text-sm text-zinc-600">
             © {new Date().getFullYear()}
             <p>Laxman K R</p>
           </span>
