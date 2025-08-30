05dc4b65cdf84bf765f2c959218932759ebc5188
Laxman K R <krlaxman03@gmail.com>
2025-08-30 23:12:02 +0530

    Refactored portfolio data fetching logic to use a new URL and added caching with revalidation. Improved error handling and added a console log for successful data fetches.

--- functions/index.ts
+++ functions/index.ts
@@ -1,13 +1,17 @@
-'use server';
-import type { PortfolioData } from '@/types';
+"use server";
+import type { PortfolioData } from "@/types";
 
 const PORTFOLIO_JSON_URL =
-  'https://gist.githubusercontent.com/thaletto/3aa83de4b93d48a4a2df4050d1d2f939/raw/e1c7efd9ce70c53c75097891a84064dd2b9be965/portfolio_data.json';
+  "https://raw.githubusercontent.com/thaletto/portfolio-data/refs/heads/main/portfolio_data.json";
 
 export async function getPortfolioData(): Promise<PortfolioData> {
-  const response = await fetch(PORTFOLIO_JSON_URL);
+  const response = await fetch(PORTFOLIO_JSON_URL, {
+    cache: "force-cache",
+    next: { revalidate: 3600 },
+  });
   if (!response.ok) {
-    throw new Error('Failed to fetch portfolio data');
+    throw new Error("Failed to fetch portfolio data");
   }
+  console.log("Fetched portfolio data", new Date().toLocaleTimeString());
   return response.json();
 }
