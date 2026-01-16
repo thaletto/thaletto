f3655382c09daa30a4512f17450b5e3c81813051
Laxman K R <krlaxman03@gmail.com>
2026-01-16 23:35:39 +0530

    fix: removed margin left in mobile view

--- components/common/block-sidetitle.tsx
+++ components/common/block-sidetitle.tsx
@@ -16,7 +16,7 @@ export function BlockSideTitle({
             </span>
             <span
                 className={cn(
-                    "sidenote block relative mt-3.5 mb-7 mx-auto text-left text-pretty w-[80%] text-xs sm:text-sm leading-5 sm:leading-6 text-rurikon-400",
+                    "sidenote block relative mt-3.5 mb-7 mx-auto text-left text-pretty text-xs sm:text-sm leading-5 sm:leading-6 text-rurikon-400",
                     "text:inline text:float-right text:clear-right text:w-[50%] text:-mr-[50%] text:mt-0 text:pl-7",
                 )}
             >
