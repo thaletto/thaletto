4de7be25c4bf74322d75f68414d8ff2802cd0e89
Laxman K R <krlaxman03@gmail.com>
2026-01-16 23:16:31 +0530

    update: Link chip uses react-icons

--- components/common/link-chip.tsx
+++ components/common/link-chip.tsx
@@ -1,26 +1,66 @@
 import Link from "next/link";
-import SvgIcon from "@/components/common/logo";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+import {
+    FaFilePdf,
+    FaFileWord,
+    FaGlobe,
+    FaGithub,
+    FaXTwitter,
+    FaLinkedin,
+    FaInstagram,
+    FaGoogleDrive,
+} from "react-icons/fa6";
+import { SiNotion } from "react-icons/si";
 
 type LinkProps = {
-    icon?: "github" | null;
+    icon?:
+        | "gdrive"
+        | "github"
+        | "globe"
+        | "instagram"
+        | "linkedin"
+        | "notion"
+        | "pdf"
+        | "word"
+        | "X"
+        | null;
     label: string;
     link: string;
 };
 
 export default function LinkChip({ icon, label, link }: LinkProps) {
+    const renderIcon = () => {
+        switch (icon) {
+            case "github":
+                return <FaGithub className="w-4 h-4" />;
+            case "globe":
+                return <FaGlobe className="w-4 h-4" />;
+            case "instagram":
+                return <FaInstagram className="w-4 h-4" />;
+            case "linkedin":
+                return <FaLinkedin className="w-4 h-4" />;
+            case "notion":
+                return <SiNotion className="w-4 h-4" />;
+            case "pdf":
+                return <FaFilePdf className="w-4 h-4" />;
+            case "word":
+                return <FaFileWord className="w-4 h-4" />;
+            case "X":
+                return <FaXTwitter className="w-4 h-4" />;
+            case "gdrive":
+                return <FaGoogleDrive className="w-4 h-4" />;
+            default:
+                return <FaGlobe className="w-4 h-4" />;
+        }
+    };
+
     return (
         <Link
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center gap-2 text-sm font-medium hover:underline"
+            className="inline-flex p-2 rounded-full items-center gap-2 text-sm font-medium hover:bg-rurikon-600 hover:text-rurikon-50 transition-colors duration-300 ease-in-out"
         >
-            {icon === "github" ? (
-                <SvgIcon name="github" width={16} height={16} />
-            ) : (
-                <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-            )}
+            {renderIcon()}
             <span>{label}</span>
         </Link>
     );
