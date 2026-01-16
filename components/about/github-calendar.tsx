410e895a2881b487a96965f18b7d847beee48541
Laxman K R <krlaxman03@gmail.com>
2026-01-16 20:31:25 +0530

    ui: removed mobile based blocksize

--- components/about/github-calendar.tsx
+++ components/about/github-calendar.tsx
@@ -1,15 +1,12 @@
-"use client";
-import { useIsMobile } from "@/hooks/use-mobile";
 import { GitHubCalendar } from "react-github-calendar";
 
 export default function GithubActivityCalendar() {
-    const isMobile = useIsMobile();
     return (
         <GitHubCalendar
             username="thaletto"
             blockMargin={2}
             blockRadius={20}
-            blockSize={isMobile ? 4 : 8}
+            blockSize={8}
             colorScheme="light"
             fontSize={12}
             maxLevel={10}
