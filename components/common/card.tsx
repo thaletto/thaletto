7529fa71c550907e34ce21e3334643473969a7f6
Laxman K R <krlaxman03@gmail.com>
2026-01-16 21:25:55 +0530

    Update card.tsx

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -17,16 +17,16 @@ export function Card({
             target="_blank"
             rel="noreferrer"
             draggable={false}
-            className="group block rounded-lg overflow-clip select-none my-7 transition-colors hover:bg-white"
+            className="group block rounded-lg overflow-clip select-none my-7 transition-colors hover:bg-white border border-rurikon-border"
         >
             <img
                 src={image}
-                className="m-0 w-full aspect-[1.9/1] object-cover border-b border-rurikon-border"
+                className="m-0 w-full aspect-[1.9/1] object-cover"
             />
-            {title && <p className="m-4 mt-3 mb-1 font-semibold">{title}</p>}
-            {desc && <p className="m-4 mb-2 mt-1 opacity-80 text-sm">{desc}</p>}
+            {title && <p className="m-2 text-sm">{title}</p>}
+            {desc && <p className="m-2 opacity-80 text-xs">{desc}</p>}
             {link && (
-                <p className="m-4 mt-1 mb-3 text-rurikon-200 text-sm transition-colors group-hover:text-rurikon-300">
+                <p className="m-2 text-rurikon-200 text-xs transition-colors group-hover:text-rurikon-300">
                     {link}
                 </p>
             )}
