e80ed8645b902c51825f3c249715fc43a61d4401
Laxman K R <krlaxman03@gmail.com>
2026-01-22 22:09:14 +0530

    fix: added pointer cursor to links in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
                 <Item href="/">
                     <Tooltip>
                         <TooltipTrigger>
-                            <span className="lowercase">About</span>
+                            <span className="lowercase cursor-pointer">About</span>
                         </TooltipTrigger>
                         <TooltipContent
                             side="right"
@@ -58,7 +58,7 @@ export default function Navbar() {
                 <Item href="/projects">
                     <Tooltip>
                         <TooltipTrigger>
-                            <span className="lowercase">Projects</span>
+                            <span className="lowercase cursor-pointer">Projects</span>
                         </TooltipTrigger>
                         <TooltipContent
                             side="right"
@@ -73,7 +73,7 @@ export default function Navbar() {
                 <Item href="/timeline">
                     <Tooltip>
                         <TooltipTrigger>
-                            <span className="lowercase">Timeline</span>
+                            <span className="lowercase cursor-pointer">Timeline</span>
                         </TooltipTrigger>
                         <TooltipContent
                             side="right"
@@ -88,7 +88,7 @@ export default function Navbar() {
                 <Item href="/writings">
                     <Tooltip>
                         <TooltipTrigger>
-                            <span className="lowercase">Writings</span>
+                            <span className="lowercase cursor-pointer">Writings</span>
                         </TooltipTrigger>
                         <TooltipContent
                             side="right"
