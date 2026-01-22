3b6c17f663a6e5cb58ff31128eea5c714d437c54
Laxman K R <krlaxman03@gmail.com>
2026-01-22 22:46:53 +0530

    ref: removed unused framer-motion import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import KeyboardShortcuts from "@/components/KeyboardShortcuts";
 import Navbar from "@/components/navbar";
 import { Analytics } from "@vercel/analytics/next";
-import { list } from "@vercel/blob";
 import cn from "clsx";
 import "katex/dist/katex.min.css";
 import type { Metadata, Viewport } from "next";
@@ -29,22 +28,6 @@ const mono = localFont({
 
 const baseURL = new URL("https://thaletto.vercel.app");
 
-async function getBlobURL() {
-    try {
-        const { blobs } = await list({ prefix: "images/opengraph/" });
-        const imageBlob = blobs.find(
-            (blob) => blob.pathname === "images/opengraph/opengraph-image.jpg",
-        );
-        return imageBlob?.url || null;
-    } catch (error) {
-        console.error("Error fetching blob URL:", error);
-        return null;
-    }
-}
-
-// Get the blob URL
-const ogURL = await getBlobURL();
-
 export const metadata: Metadata = {
     title: {
         template: "%s - Laxman K R",
@@ -53,32 +36,14 @@ export const metadata: Metadata = {
     description: "Full Stack AI Developer",
     metadataBase: baseURL,
 
-    twitter: {
-        card: "summary_large_image",
-        site: "thaletto.vercel.app",
-        creator: "@thaletto",
-        title: "thaletto Portfolio & Blog",
-        description:
-            "Portfolio & Blog website of Laxman K R, a full stack developer",
-        images: ogURL ? [ogURL] : [],
+    openGraph: {
+        type: "website",
+        siteName: "Laxman K R",
     },
 
-    openGraph: {
-        images: ogURL
-            ? [
-                  {
-                      url: ogURL,
-                      width: 1200,
-                      height: 630,
-                      alt: "Laxman K R @thaletto",
-                  },
-              ]
-            : [],
-        title: "Laxman K R @thaletto",
-        description: "Full Stack AI Developer",
-        type: "website",
-        siteName: "thaletto.vercel.app",
-        url: baseURL,
+    twitter: {
+        card: "summary_large_image",
+        creator: "@thaletto",
     },
 };
 
