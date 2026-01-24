48631ad9e060b3f5e851362f176fbe08e6959151
Laxman K R <krlaxman03@gmail.com>
2026-01-24 22:32:43 +0530

    feat: redirect to github pages resume

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,32 @@
-import withMDX from '@next/mdx'
-import { NextConfig } from 'next'
+import withMDX from "@next/mdx";
+import { NextConfig } from "next";
 
 export default withMDX()({
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  turbopack: {},
-  redirects: async () => [
-    {
-      source: '/posts/:slug',
-      destination: '/writings/:slug',
-      permanent: false,
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    turbopack: {},
+    redirects: async () => [
+        {
+            source: "/posts/:slug",
+            destination: "/writings/:slug",
+            permanent: false,
+        },
+        {
+            source: "/(resume|cv)",
+            destination: "https://thaletto.github.io/resume/cv.pdf",
+            permanent: true,
+        },
+    ],
+    experimental: {
+        mdxRs: {
+            mdxType: "gfm",
+        },
+        turbopackFileSystemCacheForDev: true,
+        turbopackFileSystemCacheForBuild: true,
     },
-  ],
-  experimental: {
-    mdxRs: {
-      mdxType: 'gfm',
+    transpilePackages: ["shiki"],
+    images: {
+        contentDispositionType: "inline",
+        contentSecurityPolicy:
+            "default-src 'self'; script-src 'none'; sandbox;",
     },
-    turbopackFileSystemCacheForDev: true,
-    turbopackFileSystemCacheForBuild: true,
-  },
-  transpilePackages: ['shiki'],
-  images: {
-    contentDispositionType: 'inline',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-} satisfies NextConfig)
+} satisfies NextConfig);
