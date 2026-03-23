8c939cc6b87213a69c1aacda3d138e8202338905
Laxman K R <krlaxman03@gmail.com>
2026-03-23 23:35:09 +0530

    init: AI Offerings

--- app/projects/[slug]/layout.tsx
+++ app/projects/[slug]/layout.tsx
@@ -4,60 +4,62 @@ import { Badge } from "@/components/ui/badge";
 import SvgIcon from "@/components/common/logo";
 
 function getCompanyLogoSrc(company?: string) {
-    if (!company) return null;
+  if (!company) return null;
 
-    switch (company.toLowerCase()) {
-        case "tcs":
-            return "/company/tcs.svg";
-        default:
-            return "/company/office.svg";
-    }
+  switch (company.toLowerCase()) {
+    case "tcs":
+      return "/company/tcs.svg";
+    case "GCP":
+      return "/company/gcp.svg";
+    default:
+      return "/company/office.svg";
+  }
 }
 
 export default async function Layout({
-    children,
-    params,
+  children,
+  params,
 }: {
-    children: ReactNode;
-    params: Promise<{ slug: string }>;
+  children: ReactNode;
+  params: Promise<{ slug: string }>;
 }) {
-    const { slug } = await params;
+  const { slug } = await params;
 
-    const { metadata } = await import(`../_projects/${slug}.mdx`);
-    const companyIcon = getCompanyLogoSrc(metadata?.company);
+  const { metadata } = await import(`../_projects/${slug}.mdx`);
+  const companyIcon = getCompanyLogoSrc(metadata?.company);
 
-    return (
-        <article className="max-w-3xl mx-auto">
-            <header className="flex flex-col">
-                <div className="flex items-center gap-2">
-                    {companyIcon && (
-                    <SvgIcon
-                        src={companyIcon}
-                        name={metadata?.company ?? ""}
-                        className="size-8 md:size-10 shrink-0 text-rurikon-400 group-hover:text-rurikon-600"
-                    />
-                    )}
-                    <h1 className="font-semibold text-xl md:text-3xl text-rurikon-600 text-balance">
-                        {metadata.title}
-                    </h1>
-                </div>
+  return (
+    <article className="max-w-3xl mx-auto">
+      <header className="flex flex-col">
+        <div className="flex items-center gap-2">
+          {companyIcon && (
+            <SvgIcon
+              src={companyIcon}
+              name={metadata?.company ?? ""}
+              className="size-8 md:size-10 shrink-0 text-rurikon-400 group-hover:text-rurikon-600"
+            />
+          )}
+          <h1 className="font-semibold text-xl md:text-3xl text-rurikon-600 text-balance">
+            {metadata.title}
+          </h1>
+        </div>
 
-                <Card image={metadata.image} title={metadata?.imageLabel} />
-                {metadata?.tags.length > 0 && (
-                    <div className="flex flex-wrap gap-1">
-                        {metadata?.tags.map((tag: string, index: number) => (
-                            <Badge
-                                key={index}
-                                variant="outline"
-                                className="text-xs px-2 py-0.5 rounded-sm"
-                            >
-                                {tag}
-                            </Badge>
-                        ))}
-                    </div>
-                )}
-            </header>
-            {children}
-        </article>
-    );
+        <Card image={metadata.image} title={metadata?.imageLabel} />
+        {metadata?.tags.length > 0 && (
+          <div className="flex flex-wrap gap-1">
+            {metadata?.tags.map((tag: string, index: number) => (
+              <Badge
+                key={index}
+                variant="outline"
+                className="text-xs px-2 py-0.5 rounded-sm"
+              >
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        )}
+      </header>
+      {children}
+    </article>
+  );
 }
