2729ded17610e9c2f0639c68622e117a57083fc1
Laxman K R <krlaxman03@gmail.com>
2026-04-23 18:40:50 +0530

    ref: Social links UI from wrapped badges to square row
    Closes A-103

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,39 +1,46 @@
 import { GitHubAchievements } from "./github-achievements";
 import Skills from "./skills";
-import LinkChip, { LinkProps } from "../common/link-chip";
 import { ContributionGraphClient } from "./contributions-graph";
 import { getContributionsData } from "@/lib/actions";
+import {
+    FaGithub,
+    FaLinkedin,
+    FaXTwitter,
+    FaInstagram,
+} from "react-icons/fa6";
+import { SiNotion } from "react-icons/si";
+import Link from "next/link";
 
 type social_link = {
     name: string;
-    icon: LinkProps["icon"];
+    icon: React.ReactNode;
     link: string;
 };
 
 const SOCIAL_LINKS: social_link[] = [
     {
         name: "GitHub",
-        icon: "github",
+        icon: <FaGithub className="size-5" />,
         link: "https://github.com/thaletto/",
     },
     {
         name: "LinkedIn",
-        icon: "linkedin",
+        icon: <FaLinkedin className="size-5" />,
         link: "https://www.linkedin.com/in/laxmanramesh/",
     },
     {
         name: "Twitter",
-        icon: "X",
+        icon: <FaXTwitter className="size-5" />,
         link: "https://x.com/thaletto",
     },
     {
         name: "Instagram",
-        icon: "instagram",
+        icon: <FaInstagram className="size-5" />,
         link: "https://instagram.com/thaletto",
     },
     {
         name: "Notion",
-        icon: "notion",
+        icon: <SiNotion className="size-5" />,
         link: "https://laxmankr.notion.site/54f72abb3cf348a7902c48a41fe0d48a?v=68aa5ecbfa7947609269a0f777fdad7f",
     },
 ];
@@ -58,16 +65,21 @@ export default async function About() {
 
             <Skills className="my-8" />
             
-            <div className="flex flex-wrap gap-2 md:gap-4 mt-8">
-                {SOCIAL_LINKS.map((item) => (
-                    <LinkChip
-                        variant="link"
-                        key={item.name}
-                        link={item.link}
-                        label={item.name}
-                        icon={item.icon}
-                    />
-                ))}
+            <div className="screen-line-top screen-line-bottom flex w-full before:z-1 after:z-1">
+                <div className="flex items-center -space-x-px">
+                    {SOCIAL_LINKS.map((item, index) => (
+                        <Link
+                            key={item.name}
+                            className="flex items-center justify-center p-3 text-muted-foreground transition-[color] hover:text-foreground first:rounded-l-md last:rounded-r-md border-y border-r border-border first:border-l"
+                            href={item.link}
+                            target="_blank"
+                            rel="noopener"
+                            aria-label={item.name}
+                        >
+                            {item.icon}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
