c899be0533809cefe5a9ca6b6d73cc991c82e22f
Laxman K R <krlaxman03@gmail.com>
2026-04-25 00:13:23 +0530

    Update mdx-components.tsx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -19,157 +19,171 @@ import { BlockMath, InlineMath } from "react-katex";
 const cssVariablesTheme = createCssVariablesTheme({});
 
 export const components: Record<
-  string,
-  (props: any) => ReactNode | Promise<ReactNode>
+    string,
+    (props: any) => ReactNode | Promise<ReactNode>
 > = {
-  h1: (props) => (
-    <h1
-      className="font-semibold text-xl md:text-3xl mb-8 text-balance"
-      {...props}
-    />
-  ),
-  h2: (props) => (
-    <h2
-      className="font-semibold text-lg md:text-2xl mt-8 text-balance"
-      {...props}
-    />
-  ),
-  h3: (props) => (
-    <h3
-      className="font-semibold text-base md:text-xl mt-8 text-balance"
-      {...props}
-    />
-  ),
-  ul: (props) => (
-    <ul
-      className="mt-2 list-disc list-outside marker:text-muted-foreground pl-5"
-      {...props}
-    />
-  ),
-  ol: (props) => (
-    <ol
-      className="mt-2 list-decimal list-outside marker:text-muted-foreground pl-5"
-      {...props}
-    />
-  ),
-  li: (props) => <li className="pl-1.5 [&>p]:mt-0" {...props} />,
-  a: ({ href, ...props }) => {
-    return (
-      <Link
-        className="wrap-break-word underline underline-offset-2 hover:text-muted-foreground focus-visible:outline focus-visible:outline-ring focus-visible:rounded-xs focus-visible:outline-offset-2"
-        href={href}
-        draggable={false}
-        {...(href?.startsWith("https://")
-          ? {
-              target: "_blank",
-              rel: "noopener noreferrer",
-            }
-          : {})}
-        {...props}
-      />
-    );
-  },
-  strong: (props) => <strong className="font-bold" {...props} />,
-  p: (props) => <p className="mt-4" {...props} />,
-  blockquote: (props) => (
-    <blockquote
-      className="pl-6 -ml-6 sm:pl-10 sm:-ml-10 md:pl-14 md:-ml-14 not-mobile:text-muted-foreground"
-      {...props}
-    />
-  ),
-  pre: (props) => (
-    <pre className="mt-4 whitespace-pre md:whitespace-pre-wrap" {...props} />
-  ),
-  code: async (props) => {
-    if (typeof props.children === "string") {
-      const code = await codeToHtml(props.children, {
-        lang: "jsx",
-        theme: cssVariablesTheme,
-        // theme: 'min-light',
-        // theme: 'snazzy-light',
-        transformers: [
-          {
-            // Since we're using dangerouslySetInnerHTML, the code and pre
-            // tags should be removed.
-            pre: (hast) => {
-              if (hast.children.length !== 1) {
-                throw new Error("<pre>: Expected a single <code> child");
-              }
-              if (hast.children[0].type !== "element") {
-                throw new Error("<pre>: Expected a <code> child");
-              }
-              return hast.children[0];
-            },
-            postprocess(html) {
-              return html.replace(/^<code>|<\/code>$/g, "");
-            },
-          },
-        ],
-      });
-
-      return (
-        <code
-          className="inline shiki css-variables rounded-md px-1.5 py-0.5 bg-muted text-foreground text-[0.805rem] sm:text-[13.8px] md:text-[0.92rem]"
-          dangerouslySetInnerHTML={{ __html: code }}
+    h1: (props) => (
+        <h1
+            className="font-semibold text-xl md:text-3xl mb-8 text-balance"
+            {...props}
         />
-      );
-    }
-
-    return <code className="inline" {...props} />;
-  },
-  img: async ({ src, alt, title }) => {
-    let img: React.ReactNode;
-
-    if (src.startsWith("https://")) {
-      img = (
-        <Image
-          className="mt-4"
-          src={src}
-          alt={alt}
-          quality={95}
-          placeholder="blur"
-          draggable={false}
+    ),
+    h2: (props) => (
+        <h2
+            className="font-semibold text-lg md:text-2xl mt-8 text-balance"
+            {...props}
         />
-      );
-    } else {
-      const image = await import(src);
-      img = (
-        <Image
-          className="mt-4"
-          src={image.default}
-          alt={alt}
-          quality={95}
-          placeholder="blur"
-          draggable={false}
+    ),
+    h3: (props) => (
+        <h3
+            className="font-semibold text-base md:text-xl mt-8 text-balance"
+            {...props}
         />
-      );
-    }
+    ),
+    ul: (props) => (
+        <ul
+            className="mt-2 list-disc list-outside marker:text-muted-foreground pl-5"
+            {...props}
+        />
+    ),
+    ol: (props) => (
+        <ol
+            className="mt-2 list-decimal list-outside marker:text-muted-foreground pl-5"
+            {...props}
+        />
+    ),
+    li: (props) => (
+        <li className="pl-1.5 [&>p]:mt-0 text-muted-foreground" {...props} />
+    ),
+    a: ({ href, ...props }) => {
+        return (
+            <Link
+                className="wrap-break-word underline underline-offset-2 text-muted-foreground hover:text-foreground focus-visible:outline focus-visible:outline-ring focus-visible:rounded-xs focus-visible:outline-offset-2"
+                href={href}
+                draggable={false}
+                {...(href?.startsWith("https://")
+                    ? {
+                          target: "_blank",
+                          rel: "noopener noreferrer",
+                      }
+                    : {})}
+                {...props}
+            />
+        );
+    },
+    strong: (props) => <strong className="font-bold" {...props} />,
+    p: (props) => (
+        <p className="mt-4 font-normal text-muted-foreground" {...props} />
+    ),
+    blockquote: (props) => (
+        <blockquote
+            className="pl-6 -ml-6 sm:pl-10 sm:-ml-10 md:pl-14 md:-ml-14 not-mobile:text-muted-foreground"
+            {...props}
+        />
+    ),
+    pre: async (props) => {
+        const codeEl = props.children;
 
-    if (title) {
-      return <BlockSideTitle title={title}>{img}</BlockSideTitle>;
-    }
+        // Safety check
+        if (typeof codeEl !== "object" || !("props" in codeEl)) {
+            return (
+                <pre
+                    className="mt-4 whitespace-pre md:whitespace-pre-wrap"
+                    {...props}
+                />
+            );
+        }
 
-    return img;
-  },
-  hr: (props) => <hr className="my-14 w-24 border" {...props} />,
-  Card,
-  Image,
-  Code,
-  BlockSideTitle,
-  Callout,
-  InlineMath,
-  BlockMath,
-  ViewCount,
-  LinkChip,
-  Mermaid,
-  Hero,
-  About,
-  ThatsWhatSheSaid,
+        const rawCode = codeEl.props.children;
+        const lang = codeEl.props.className?.replace("language-", "") || "text";
+
+        const html = await codeToHtml(String(rawCode), {
+            lang,
+            theme: cssVariablesTheme,
+            transformers: [
+                {
+                    pre: (hast) => {
+                        const child = hast.children[0];
+                        if (child && child.type === "element") {
+                            return child;
+                        }
+                        return hast
+                    },
+                    postprocess: (html) =>
+                        html.replace(/^<code>|<\/code>$/g, ""),
+                },
+            ],
+        });
+
+        return (
+            <pre className="mt-4 overflow-x-auto rounded-lg p-4 bg-shiki-background text-shiki-foreground">
+                <code
+                    className="shiki css-variables"
+                    dangerouslySetInnerHTML={{ __html: html }}
+                />
+            </pre>
+        );
+    },
+    code: (props) => {
+        return (
+            <code className="inline rounded-md px-1.5 py-0.5 bg-shiki-background text-shiki-foreground text-[0.9rem]">
+                {props.children}
+            </code>
+        );
+    },
+    img: async ({ src, alt, title }) => {
+        let img: React.ReactNode;
+
+        if (src.startsWith("https://")) {
+            img = (
+                <Image
+                    className="mt-4"
+                    src={src}
+                    alt={alt}
+                    quality={95}
+                    placeholder="blur"
+                    draggable={false}
+                />
+            );
+        } else {
+            const image = await import(src);
+            img = (
+                <Image
+                    className="mt-4"
+                    src={image.default}
+                    alt={alt}
+                    quality={95}
+                    placeholder="blur"
+                    draggable={false}
+                />
+            );
+        }
+
+        if (title) {
+            return <BlockSideTitle title={title}>{img}</BlockSideTitle>;
+        }
+
+        return img;
+    },
+    hr: (props) => <hr className="my-14 w-24 border" {...props} />,
+    Card,
+    Image,
+    Code,
+    BlockSideTitle,
+    Callout,
+    InlineMath,
+    BlockMath,
+    ViewCount,
+    LinkChip,
+    Mermaid,
+    Hero,
+    About,
+    ThatsWhatSheSaid,
 };
 
 export function useMDXComponents(inherited: MDXComponents): MDXComponents {
-  return {
-    ...inherited,
-    ...(components as any),
-  };
+    return {
+        ...inherited,
+        ...(components as any),
+    };
 }
