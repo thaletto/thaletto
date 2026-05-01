6ff3b21e7e19e0557fc9f8b2f633865393a82abf
Laxman K R <krlaxman03@gmail.com>
2026-05-01 13:50:22 +0530

    build error fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,10 @@
-import NotFoundContent from "./_contents/not-found.mdx";
-
 export default function NotFound() {
-	return <NotFoundContent />;
+	return (
+		<>
+			<h1 className="mb-8 text-balance font-semibold text-xl md:text-3xl">
+				404 - Not Found
+			</h1>
+			<img src="/rick-roll.gif" />
+		</>
+	);
 }
