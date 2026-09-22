9001ea1f62f3529f0d9dcc0e666ececbc51aaf5a
thaletto <krlaxman03@gmail.com>
2026-09-22 20:27:00 +0530

    style: reflow contact sentence in home introduction

--- src/components/home/home-introduction.tsx
+++ src/components/home/home-introduction.tsx
@@ -71,8 +71,8 @@ function HomeContact({ social, github }: { social: SocialSnapshot; github: GitHu
         trigger={`@${siteSocial.x.handle}`}
         triggerClassName="home-contact-link"
       />
-      , or on <GitHubCard data={github} trigger="GitHub" triggerClassName="home-contact-link" />. Email
-      me at{' '}
+      , or on <GitHubCard data={github} trigger="GitHub" triggerClassName="home-contact-link" />.
+      Email me at{' '}
       <EmailCard
         identity={siteIdentity}
         trigger={siteIdentity.email}
