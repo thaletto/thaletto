32cb504a1aedf4467e05cf25a24d0582e5ed54b3
thaletto <krlaxman03@gmail.com>
2026-09-23 23:28:29 +0530

    refactor: export WayfindingArrow from post-toc for reuse

--- src/components/blog/post-toc.tsx
+++ src/components/blog/post-toc.tsx
@@ -16,7 +16,7 @@ function getReadingTop(target: HTMLElement) {
   return target.getBoundingClientRect().top
 }
 
-function WayfindingArrow({ direction }: { direction: 'back' | 'top' }) {
+export function WayfindingArrow({ direction }: { direction: 'back' | 'top' }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" aria-hidden>
       <g
